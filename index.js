a0ed78156731145c0c5e4a03b73e0de6edb69418
Roshan Lasiwa <roshanlasiwa@gmail.com>
2021-05-31 21:40:20 +0545

    remove console log

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ setInterval(() => {
     mrotation= 6*mtime;
     srotation=6*stime;
 
-    console.log(htime,mtime,stime,hour,hrotation,mrotation,srotation,second);
+    // console.log(htime,mtime,stime,hour,hrotation,mrotation,srotation,second);
     //hour.style.transform = `rotate ( ${hrotation}deg)`;
     // hour.style.transform = `rotate(${hrotation})`;
     // minute.style.transform = `rotate ( ${mrotation}deg)`;
